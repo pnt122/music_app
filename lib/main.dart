63d8b6a295f35fd3a1d7d3c7e0d7484ddc9ed9b9
thanh12a12002 <phamngocthanh12a12002@gmail.com>
2024-06-29 00:17:22 +0700

    Create project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
